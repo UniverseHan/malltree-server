8887a515f89e8742c15e98b62471968beb2eda9b
jkparkGitHub <pjklogs@naver.com>
2016-10-09 13:37:51 +0900

    item 추가 , item 목록 구현

--- src/main/java/com/example/test/testController.java
+++ src/main/java/com/example/test/testController.java
@@ -1,5 +1,7 @@
 package com.example.test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,31 +13,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class testController {
 
-    //GET방식
-    private static final String template = "http://test12.dothome.co.kr/images/%s";
-    private final AtomicLong counter = new AtomicLong();
+  //GET방식
+  private static final String template = "http://test12.dothome.co.kr/images/%s";
+  private final AtomicLong counter = new AtomicLong();
+  
+  //Array
+  ArrayList<Item> items = new ArrayList<Item>();
 
-    @RequestMapping("/test")
-    public test greeting(
-    		@RequestParam(value="name", defaultValue="on.png") String name,
-    		@RequestParam(value="age", defaultValue="20") Integer age) 
-    {
-        return new test(
-        		counter.incrementAndGet(),
-        		String.format(template, name),
-        		age);
-    }
+  @RequestMapping("/test")
+  public test greeting(
+      @RequestParam(value="name", defaultValue="on.png") String name,
+      @RequestParam(value="age", defaultValue="20") Integer age) 
+  {
+    return new test(
+        counter.incrementAndGet(),
+        String.format(template, name),
+        age);
+  }
+  
+  @RequestMapping("/items")
+  public List<Item> getItemList() {
+    return items;
+  }
+
+
+  //POST방식
+  @PostMapping(path = "/test", consumes = "application/json")
+  public String post(@RequestBody AddItemRequest request) {
+    // implementation omitted
     
+    
+    items.add( new Item(request.getName(), request.getPrice()));
+    
+    System.out.println("number of items is " + items.size());
 
-    //POST방식
-	@PostMapping(path = "/test", consumes = "application/json")
-	public String post(@RequestBody String name, String age) {
-	    // implementation omitted
+    //Array에 입력
+    return "(Name) : " + request.getName() + "   (age) : " + request.getPrice();
 
-        //Array에 입력
-        return "(Name) : " + name + "   (age) : " + age;
-        
-	}
+  }
 
 }
 
